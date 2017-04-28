c9f9da57c4ae3440cf098e5f398800bafef270ab
Unknown <nolse16@student.aau.dk>
2017-04-28 03:12:06 +0200

    changes

--- PollvotingSoftware/app/src/main/java/com/example/christian/pollvotingsoftware/Create_Poll.java
+++ PollvotingSoftware/app/src/main/java/com/example/christian/pollvotingsoftware/Create_Poll.java
@@ -8,6 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import static com.example.christian.pollvotingsoftware.Main_Menu.messageFromServer;
+import static com.example.christian.pollvotingsoftware.Main_Menu.messageToServer;
+
 /**
  * Created by Christian on 27-04-2017.
  */
@@ -32,12 +42,56 @@ public class Create_Poll extends AppCompatActivity {
     }
 
     public void savePoll(View view){
-        String finalDescription = description.getText().toString();
-        String finalOption1 = option1.getText().toString();
-        String finalOption2 = option2.getText().toString();
-        String finalPoll = "0," + "0," + finalDescription + "," + finalOption1 + "," + finalOption2;
 
-        Log.d("done", finalPoll);
+
+
+        //start server
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    try {
+                        String finalDescription = description.getText().toString();
+                        String finalOption1 = option1.getText().toString();
+                        String finalOption2 = option2.getText().toString();
+                        String finalPoll = "newPoll" + "0," + "0," + finalDescription + "," + finalOption1 + "," + finalOption2;
+
+                        InetAddress IP = InetAddress.getByName("10.0.2.2");
+                        Socket clientSocket = new Socket(IP, 4445);
+                        Log.d("Servertesting", "creates socket");
+                        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                        Log.d("Servertesting", "establishes outputstream");
+                        outToServer.writeBytes(finalPoll + "\n");
+                        Log.d("SENT", "Message is sent to server");
+//
+//                        //waits and read input from, maybe not necessary in all cases
+                        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        Log.d("From server", "received shit from server");
+                        //modified sentence == results from server
+                        messageFromServer = inFromServer.readLine();
+                        Log.d("From server", messageFromServer);
+                        clientSocket.close();
+                        outToServer.close();
+                        inFromServer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //Your code goes here
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+            Log.d("finishthread", "thread ends");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+//        Log.d("done", finalPoll);
     }
 
 
